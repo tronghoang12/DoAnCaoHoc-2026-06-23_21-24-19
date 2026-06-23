ad5d14b2d2d85e2375fc3848ff283797f7a42ca6
Trongdh <trongdo2411@gmail.com>
2026-06-23 22:09:07 +0700

    Update 23/06

--- Assets/Script/Check.cs
+++ Assets/Script/Check.cs
@@ -1,7 +1,15 @@
+using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Check : MonoBehaviour
 {
+    string TxtKeyWord;
+    GameObject _mainCamera;
+    GameObject _target;
+    GameObject _gb2ShowRoom;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +21,30 @@ public class Check : MonoBehaviour
     {
         
     }
+    public bool CheckingTxtKeyWord(string TxtInit)
+    {
+        //tạo biểu thực regex để kiểm tra toán tử
+        string pattern = $@"\b{Regex.Escape(TxtKeyWord)}\b";
+        
+        //kiểm tra toán tử (Không phân biệt hoa thường)
+        if (Regex.IsMatch(TxtInit, pattern, RegexOptions.IgnoreCase))
+        {
+            Debug.Log($"Chuỗi có chứa chính xác từ '{TxtKeyWord}'(Không phân biệt hoa thường).");
+
+            if(TxtKeyWord == "Information")
+            {
+                _mainCamera.SetActive(false);
+                _mainCamera.transform.position = new Vector3(_target.transform.position.x, _mainCamera.transform.position.y, _target.transform.position.z);
+                _mainCamera.transform.eulerAngles = new Vector3(0,0,0);
+                _gb2ShowRoom.SetActive(true);
+            }
+            return true;
+        }
+
+        Debug.Log($"Chuỗi không chứa từ '{TxtKeyWord}'.");
+        return false;
+
+    }
 }
+
+
